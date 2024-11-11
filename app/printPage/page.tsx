d6b4bc662b716610f4faf7821e0296a0083e4497
qh20166 <164975572+qh20166@users.noreply.github.com>
2024-11-11 13:24:24 +0700

    Add files via upload

--- app/printPage/page.tsx
+++ app/printPage/page.tsx
@@ -1,5 +1,16 @@
-const PrintPage = () => {
-    return <div>Hello Printing paeg</div>;
+import React from 'react';
+import UploadSection from './UploadSection';
+import UploadedFilesList from './UploadedFilesList';
+import './styles.css'
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <div className="main-content">
+        <UploadSection />
+        <UploadedFilesList />
+      </div>
+    </div>
+  );
 };
 
-export default PrintPage;
+export default App;
