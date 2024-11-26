09a3153014855add9749a79e45dee68f88a717df
Kien-Tran <ttkien2004@gmail.com>
2024-11-26 10:32:29 +0700

    remove pre-login page

--- app/pre-login/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PreLoginLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return <div>{children}</div>;
-};
-
-export default PreLoginLayout;
